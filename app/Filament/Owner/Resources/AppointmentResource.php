269393fb35a4a89a0f493d1f6588f9dd3ecef7a2
PatrickMamsery <pkrobert1612@gmail.com>
2024-05-08 15:08:24 +0300

    wip: add navigation to the various panels

--- app/Filament/Owner/Resources/AppointmentResource.php
+++ app/Filament/Owner/Resources/AppointmentResource.php
@@ -2,21 +2,23 @@
 
 namespace App\Filament\Owner\Resources;
 
-use App\Enums\AppointmentStatus;
-use App\Filament\Owner\Resources\AppointmentResource\Pages;
-use App\Models\Appointment;
-use App\Models\Role;
-use App\Models\User;
-use App\Support\AvatarOptions;
 use Carbon\Carbon;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Forms\Get;
-use Filament\Resources\Resource;
+use App\Models\Role;
+use App\Models\User;
 use Filament\Tables;
+use Filament\Forms\Get;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
+use App\Models\Appointment;
+use App\Support\AvatarOptions;
+use App\Enums\AppointmentStatus;
+use Filament\Resources\Resource;
+use Illuminate\Support\Collection;
 use Illuminate\Support\HtmlString;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Owner\Resources\AppointmentResource\Pages;
+use App\Models\Slot;
 
 class AppointmentResource extends Resource
 {
@@ -27,18 +29,22 @@ class AppointmentResource extends Resource
     public static function form(Form $form): Form
     {
         $doctorRole = Role::whereName('doctor')->first();
-        
+
         return $form
             ->schema([
                 Forms\Components\Select::make('pet_id')
+                    ->relationship('pet', 'name')
                     ->required(),
                 Forms\Components\Select::make('clinic_id')
+                    ->relationship('clinic', 'name')
                     ->required(),
-                    Forms\Components\DatePicker::make('date')
+                Forms\Components\DatePicker::make('date')
+                    ->live()
                     ->required(),
                 Forms\Components\Select::make('doctor_id')
                     ->required()
                     ->hidden(fn (Get $get) => blank($get('date')))
+                    ->live()
                     ->allowHtml()
                     ->options(function (Get $get) use ($doctorRole) {
                         $doctors = User::whereBelongsTo($doctorRole)
@@ -47,15 +53,17 @@ class AppointmentResource extends Resource
                                 $query
                                     ->where('day_of_week', $dayOfTheWeek)
                                     ->where('clinic_id', $get('clinic_id'));
-
                             })
                             ->get();
                         return $doctors->mapWithKeys(function ($doctor) {
                             return [$doctor->getKey() => AvatarOptions::getOptionString($doctor)];
                         })->toArray();
                     })
+                    ->afterStateUpdated(function (Forms\Set $set) {
+                        $set('slot_id', null);
+                    })
                     ->helperText(function ($component) {
-                        if (! $component->getOptions()) {
+                        if (!$component->getOptions()) {
                             return new HtmlString(
                                 '<span class="text-sm text-danger-600 dark:text-danger-400">No doctors available. Please select a different clinic or date</span>'
                             );
@@ -64,6 +72,13 @@ class AppointmentResource extends Resource
                         return '';
                     }),
                 Forms\Components\Select::make('slot_id')
+                    ->label('Slot')
+                    ->options(fn (Forms\Get $get): Collection => Slot::query()
+                        ->where('schedule_id', $get('schedule_id'))
+                        ->pluck('start', 'end', 'id'))
+                    ->preload()
+                    ->live()
+                    ->searchable()
                     ->required(),
                 Forms\Components\TextInput::make('description')
                     ->required(),
@@ -113,14 +128,14 @@ class AppointmentResource extends Resource
                 ]),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -128,5 +143,5 @@ class AppointmentResource extends Resource
             'create' => Pages\CreateAppointment::route('/create'),
             'edit' => Pages\EditAppointment::route('/{record}/edit'),
         ];
-    }    
+    }
 }
